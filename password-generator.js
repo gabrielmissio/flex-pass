865fa0111b111f13f84f7317936805ef2a97f4f4
gabrielmissio <gabrielmissio.dev@gmail.com>
2023-04-29 08:56:00 -0500

    refactor: rename variables and add comments to improve the readability

--- password-generator.js
+++ password-generator.js
@@ -1,37 +1,38 @@
-class PasswordGenerator {
-  static generate(
-    { numbers, upperCase, lowerCase, specials } = { numbers: 2, upperCase: 2, lowerCase: 3, specials: 1 }
-  ) {
-    const numbersPool = '0123456789';
-    const upperCasePool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const lowerCasePool = 'abcdefghijklmnopqrstuvwxyz';
-    const specialCharsPool = '.+-*/!@#$';
-    const passwordLength = numbers + upperCase + lowerCase + specials;
+// Define pools of characters to be used to generate password
+const numericCharacters = '0123456789'
+const uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz'
+const specialCharacters = '.+-*/!@#$'
 
-    let password = '';
-    password += this.characterShuffler({ pool: numbersPool, desiredNumberOfCharacters: numbers });
-    password += this.characterShuffler({ pool: upperCasePool, desiredNumberOfCharacters: upperCase });
-    password += this.characterShuffler({ pool: lowerCasePool, desiredNumberOfCharacters: lowerCase });
-    password += this.characterShuffler({ pool: specialCharsPool, desiredNumberOfCharacters: specials });
+function generatePassword({ numbers = 2, upperCase = 2, lowerCase = 3, specials = 1 } = {}) {
+  // Generate password by shuffling characters from each pool
+  let newPassword = ''
+  newPassword += shuffleCharacters(numericCharacters, numbers)
+  newPassword += shuffleCharacters(uppercaseCharacters, upperCase)
+  newPassword += shuffleCharacters(lowercaseCharacters, lowerCase)
+  newPassword += shuffleCharacters(specialCharacters, specials)
+  
+  // Calculate the total length of the password
+  const desiredLength = numbers + upperCase + lowerCase + specials
 
-    return this.characterShuffler({ pool: password, desiredNumberOfCharacters: passwordLength });
-  }
+  // Shuffle the password one more time to make it even more random
+  newPassword = shuffleCharacters(newPassword, desiredLength)
 
-  static characterShuffler({ pool, desiredNumberOfCharacters }) {
-    if (!pool) throw new Error('MissingParamError: pool');
-    if (!desiredNumberOfCharacters) throw new Error('MissingParamError: desiredNumberOfCharacters');
-
-    const insufficientPool = pool.length < desiredNumberOfCharacters;
-    const currentNumberOfCharacters = insufficientPool ? pool.length : desiredNumberOfCharacters;
-
-    const response = pool
-      .split('')
-      .sort(() => 0.5 - Math.random())
-      .join('')
-      .substr(0, currentNumberOfCharacters);
-
-    return response;
-  }
+  return newPassword
 }
 
-console.log(PasswordGenerator.generate());
+function shuffleCharacters(characters, desiredLength) {
+  if (!characters) throw new Error('MissingParamError: characters')
+  if (isNaN(desiredLength)) throw new Error('MissingParamError: desiredLength')
+
+  // Return empty string when desiredLength is equal to 0
+  if (desiredLength === 0) return ''
+
+  // Calculate the actual number of characters to be shuffled
+  const actualLength = Math.min(characters.length, desiredLength)
+
+  // Shuffle the characters by converting the string into an array, shuffling it and converting it back to a string
+  const randomizedCharacters = characters.split('').sort(() => Math.random() - 0.5).slice(0, actualLength).join('')
+
+  return randomizedCharacters
+}
